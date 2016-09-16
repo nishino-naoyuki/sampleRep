f6468e64dcb88462f1c7abff93df6c13bafbb033
student <student@PC634S41.asost.local>
2016-09-16 15:09:22 +0900

    test

--- src/jp/ac/asojuku/test/Test.java
+++ src/jp/ac/asojuku/test/Test.java
@@ -9,4 +9,13 @@ package jp.ac.asojuku.test;
  */
 public class Test {
 	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
 }
